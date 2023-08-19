b47c516768ed3574d90d22dd7f94585ff00cb889
ntt256 <cvathanh.tran@gmail.com>
2023-08-18 22:30:41 -0400

    I added my Pre-Work files to my repository.

---
+++ Nhut PreWork/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Nhut PreWork
+//
+//  Created by Tay Yeu on 8/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        func changeColor() -> UIColor{
+            
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+
+
+        }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+}
+
